f7382afa2fbc589929c3ebc8ba336ea39584ef49
alexkarav <sashagameac@gmail.com>
2025-08-22 23:29:05 +0300

    Готовый проект

--- app/db/session/db_session.py
+++ app/db/session/db_session.py
@@ -1,13 +1,12 @@
-from dotenv import load_dotenv
-from os import environ
+from environs import Env
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from core.config import dev_settings, prod_settings
 
-load_dotenv()
+env = Env()
 
+debug = env.bool("DEBUG")
 
-debug = bool(environ.get("DEBUG"))
 engine = create_engine(
     prod_settings.DATABASE_URI if not debug else dev_settings.DATABASE_URI
 )
